3b3d36d96ce6f2afc4c5a03d93d8d79321eb1cec
madhavan <madhavanramlingam@gmail.com>
2024-08-04 20:29:17 +0530

    Adding if condition by dev 2

--- src/main/java/Pgms/Revers.java
+++ src/main/java/Pgms/Revers.java
@@ -3,17 +3,19 @@ package Pgms;
 public class Revers {
 
 	public static void main(String[] args) {
+		String str = "Text";
 		//Dev 2  iam adding some line of code
 		System.out.println("Welcome");
 		System.out.println("condition");
 		System.out.println("condition1");
 		//Adding test one
 		//Test 2
-       //Dev one Adding or Modify the code
+		//Dev one Adding or Modify the code
 		System.out.println("Added  from master syso1");
 		System.out.println("Added  from master sys2");
 		System.out.println("Added  from master sys3");
-		System.out.println("Added  from master sys4");
+		System.out.println("Added  from master sys4 changes By dev 2");
+		//Welcome Dev 2 changes
 
 		//I am Adding the Code
 
@@ -21,6 +23,9 @@ public class Revers {
 
 		//Dev 2  iam adding  new logic code
 
-	}
+		if (str.contains("Welcome")) {
+			System.out.println("Print Nice");
+		}
 
+	}
 }
