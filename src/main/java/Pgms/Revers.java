27beecbcb3ab8821070dc64a5c41bfc3bcf012ce
jdmadhavan <48202812+jdmadhavan@users.noreply.github.com>
2024-08-04 20:20:50 +0530

    Feature one update by dev one

--- src/main/java/Pgms/Revers.java
+++ src/main/java/Pgms/Revers.java
@@ -3,6 +3,7 @@ package Pgms;
 public class Revers {
 
 	public static void main(String[] args) {
+		//Declaration by dev one
 		//Dev 2  iam adding some line of code
 		System.out.println("Welcome");
 		System.out.println("condition");
@@ -13,13 +14,15 @@ public class Revers {
 		System.out.println("Added  from master syso1");
 		System.out.println("Added  from master sys2");
 		System.out.println("Added  from master sys3");
-		System.out.println("Added  from master sys4");
+		System.out.println("Added  from master sys4 dev one changes");
 
 		//I am Adding the Code
 
 		//Dev 2 iam adding some code changes in different file
 
 		//Dev 2  iam adding  new logic code
+
+		String s="Welcome Tester world" // added by dev one
 
 	}
 
