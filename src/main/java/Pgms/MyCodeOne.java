f98161830b4b9be31ed2d69906c5b872eb86770e
jdmadhavan <48202812+jdmadhavan@users.noreply.github.com>
2024-10-19 19:15:54 +0530

    Update MyCodeOne.java
    added mycode tc02 implmentation

--- src/main/java/Pgms/MyCodeOne.java
+++ src/main/java/Pgms/MyCodeOne.java
@@ -2,6 +2,7 @@ package Pgms;
 
 public class MyCodeOne {
     public static void main(String[] args) {
+        System.out.println("Before line added");
         System.out.println("Added one file");
         System.out.println("My change 1");
 
