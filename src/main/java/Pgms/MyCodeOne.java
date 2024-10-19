58ebad650d925d9ed3dee6c28179ff5a5fc06ad2
jdmadhavan <madhavanramlingam@gmail.com>
2024-10-19 19:34:00 +0530

    same file added

--- src/main/java/Pgms/MyCodeOne.java
+++ src/main/java/Pgms/MyCodeOne.java
@@ -2,6 +2,7 @@ package Pgms;
 
 public class MyCodeOne {
     public static void main(String[] args) {
+        System.out.println("My change  from sprint new");
         System.out.println("Added one file");
         System.out.println("My change 1");
 
